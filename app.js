4a612c18fcf7910c5afba8525832923e2c0d8755
Duarte Almeida <duartemalmeida@gmail.com>
2021-06-15 14:23:09 +0100

    complete the js logic - bug around text content to be fixed

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ window.addEventListener('load', () => {
             lon = position.coords.longitude
             lat = position.coords.latitude
             mainTitle.textContent = 'Fetching your location data..'
+
             //API call
             const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${KEY}`
 
@@ -77,14 +78,70 @@ form.addEventListener('submit', e => {
     let inputVal = input.value;
 
     //confirm if city is already displayed
-    const listItems = list.querySelector('.city')
+    const listItems = list.querySelectorAll('#results_field .city')
     const listArray = Array.from(listItems)
 
     if (listArray.length > 0) {
         const filteredArray = listArray.filter(item => {
+            let content = ""
+            if (inputVal.includes(',')) {
+                if (inputVal.split(',')[1].length > 2) {
+                    inputVal = inputVal.split(',')[0]
+                    content = item
+                        .querySelector('city-name span')
+                        .textContent.toLowerCase()
+                } else {
+                    content = item.querySelector('city-name').dataset.name.toLowerCase()
+                }
+            } else {
+                content = item.querySelector('city-name span').textContent.toLowerCase()
+            }
+            return content = inputVal.toLowerCase()
+            })
 
-        })
+        if (filteredArray.length > 0) {
+            mainTitle.textContent = `You already know the weather for ${
+                filteredArray[0].querySelector('city-name span').textContent},
+                if you are refering to a different city please provide the country code as well`
+                form.reset()
+                input.focus()
+                return 
+        }
     }
-    })
+    //API Call
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${KEY}&units=metric`
 
+    const weatherData = async () => {
+        let response = await fetch('http://' + api)
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+        let weather = await response.json()
+        return weather
+    }
+
+    weatherData()
+        .then(data => {
+            //set variables
+            const { main, name, sys, weather } = data
+            const icon = `https://openweathermap.org/img/wn/${
+                weather[0]["icon"]
+                }@2x.png`;
+            //change main title
+            mainTitle.textContent = 'Search for another city below'
+            //create list and add mark up
+            const li = document.createElement('li')
+            li.classList.add('city')
+
+            li.innerHTML = content(name, sys.country, main.temp, icon, weather[0]['description'])
+            list.appendChild(li)
+            })
+        .catch(e => {
+            mainTitle.textContent = `Please search for a valid city!`
+        })
+    mainTitle.textContent = 'Search for another city below'
+    form.reset()
+    input.focus()
+})
 
